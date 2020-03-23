0fa12887d8efd4c2122ea8ccf378fa9911176200
H. Watanabe <kaityo@users.sourceforge.jp>
2020-03-24 01:12:19 +0900

    updates

--- intersect/find_intersect.rb
+++ intersect/find_intersect.rb
@@ -9,7 +9,7 @@ def intersect?(p1, p2, p3, p4)
   t2 = f(p1, p2, p4)
   t3 = f(p3, p4, p1)
   t4 = f(p3, p4, p2)
-  t1 * t2 < 0.0 and t3 * t4
+  t1 * t2 < 0.0 and t3 * t4 < 0.0
 end
 
 def intersection(p1, p2, p3, p4)
@@ -19,3 +19,34 @@ def intersection(p1, p2, p3, p4)
   y = t * p1.y + (1.0 - t) * p2.y
   return x, y
 end
+
+def read_file(filename)
+  File.read(filename).split(/\n/).map do |n|
+    x, y = n.split(/\s+/)
+    Point.new(x.to_f, y.to_f)
+  end
+end
+
+def find_intersection(file1, file2)
+  puts "reading #{file1}"
+  data1 = read_file(file1)
+  puts "reading #{file2}"
+  data2 = read_file(file2)
+  (data1.size - 1).times do |i|
+    p1 = data1[i]
+    p2 = data1[i + 1]
+    (data2.size - 1).times do |j|
+      p3 = data2[j]
+      p4 = data2[j + 1]
+      if intersect?(p1, p2, p3, p4)
+        return intersection(p1, p2, p3, p4)
+      end
+    end
+  end
+end
+
+open("intersection.dat", "w") do |f|
+  x, y = find_intersection("data1.dat", "data2.dat")
+  f.puts "#{x} #{y}"
+end
+puts "generated intersection.dat"
--- intersect/makedata.rb
+++ intersect/makedata.rb
@@ -1,5 +1,5 @@
-f1 = open("test1.dat", "w")
-f2 = open("test2.dat", "w")
+f1 = open("data1.dat", "w")
+f2 = open("data2.dat", "w")
 10.times do |i|
   x1 = i + rand * 0.2 - 0.1
   y1 = x1 ** 2.0 + 10.0 + rand * 0.2 - 0.1
@@ -8,3 +8,6 @@ f2 = open("test2.dat", "w")
   y2 = 0.3 * x2 ** 3.0
   f2.puts "#{x2} #{y2}"
 end
+
+puts "generated data1.dat"
+puts "generated data2.dat"
